c1c4bc5a8b12882ea9513d64b7154ca4f0e33a89
David Barnes <david.barnes@manchester.ac.uk>
2020-08-24 13:41:16 -0400

    Add the constants file I forgot

---
+++ constants.py
@@ -0,0 +1,26 @@
+# Set constants for analysis consistency!
+
+# Mass of Sun in grams
+Msun_g = 1.989e33
+# TNG mass unit in solar masses
+Mtng_Msun = 1.0e10
+# Megaparsec in cm
+Mpc_cm = 3.086e24
+# Kiloparsec in cm
+kpc_cm = 3.086e21
+# km in cm
+km_cm = 1.0e5
+# Boltzmann constant
+kB_erg_K = 1.3807e-16
+# Convert keV to erg
+kev_2_erg = 1.60215e-9
+# Proton mass in grams
+mp_g = 1.67262e-24
+# Electron rest-mass in keV
+me_keV = 511.0
+# Mean molecular weight
+mu = 0.59
+# Thompson cross section in cm^2
+sigT = 6.65246e-25
+# TNG solar metallicity conversion (unable to verify this value!)
+Ztng_solar = 0.0127
