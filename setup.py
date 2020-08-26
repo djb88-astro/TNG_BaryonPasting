08f7a83ce6b0300371520113bb07768424b8b5fd
David Barnes <djbarnes@mit.edu>
2020-08-26 15:03:51 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="bp_tng_extraction",
+    version="3.0.1",
+    description="TNG halo properties calculations for Baryon Pasters",
+    url="https://github.com/djb88-astro/TNG_BaryonPasting",
+    author="David J Barnes",
+    author_email="djbarnes@mit.edu",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["numpy", "scipy", "h5py", "astropy", "mpi4py", "numba"],
+)
